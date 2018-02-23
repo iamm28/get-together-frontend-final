955e5718d05882f7e73bbd54562fa44391445ec7
iamm28 <alisonmackay246@gmail.com>
2018-02-23 14:28:21 -0500

    login form

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const LogIn = (props) => {
   return (
-    <h3>Log In</h3>
+    <div>
+      <form>
+        <div>
+          <h3>Log In</h3>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
   )
 }
 
